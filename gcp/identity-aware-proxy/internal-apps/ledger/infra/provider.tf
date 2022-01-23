b27889fa7b8dcb423ecfc9ca5030ac869ebfecd9
Alex Woods <alexhwoods@gmail.com>
2022-01-23 16:47:10 -0500

    feat: use cluster with working ingress

--- gcp/identity-aware-proxy/internal-apps/ledger/infra/provider.tf
+++ gcp/identity-aware-proxy/internal-apps/ledger/infra/provider.tf
@@ -1,4 +1,4 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
-  config_context = "gke_identity-4430bfc_us-central1_internal-application-4430bfc"
+  config_context = "gke_identity-4430bfc_us-central1-c_cluster-1"
 }
